95aaaaa90bdc052ce80da5efbaa58116ecf29e8f
MW <MW@DESKTOPDELL>
2019-01-06 01:09:08 -0800

    dev

---
+++ CMEngineCore/StateManager.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace CMEngineCore
+{
+    public class StateManager
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Archive()
+        {
+            string savdDir = ConfigurationManager.AppSettings["SaveFolder"];
+            DirectoryInfo dir = new DirectoryInfo(savdDir);
+            if (!dir.Exists) Directory.CreateDirectory(savdDir);
+
+            if (!Directory.Exists(string.Format("{0}\\{1}", savdDir, "archive")))
+                Directory.CreateDirectory(string.Format("{0}\\{1}", savdDir, "archive"));
+
+            foreach(FileInfo file in dir.GetFiles())
+            {
+                string newName = string.Format("{0}\\{1}\\{2}", savdDir, "archive", Path.GetFileNameWithoutExtension(file.Name) + "_" + DateTime.Now.Ticks + ".dat");
+                File.Copy(file.FullName, newName);
+            }
+        }
+
+    }
+}
